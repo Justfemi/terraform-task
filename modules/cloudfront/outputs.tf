16f8b09704be6115d4d3e14031c70d5cf4565804
Justfemi <ajibade.olufemi@yahoo.com>
2024-06-29 03:38:23 +0100

    ft:updated all files

---
+++ modules/cloudfront/outputs.tf
@@ -0,0 +1,7 @@
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.static_cloudfront.domain_name
+}
+
+output "cloudfront_hosted" {
+  value = aws_cloudfront_distribution.static_cloudfront.hosted_zone_id
+}
